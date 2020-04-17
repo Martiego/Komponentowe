12a2ef3e87ef1a48a6407d1c1a037639a3c8aa81
Kacper Świercz <kacper.swiercz00@gmail.com>
2020-04-17 21:10:07 +0200

    Edit Window class

--- src/pl/komponentowe/Window.java
+++ src/pl/komponentowe/Window.java
@@ -1,14 +1,4 @@
 package pl.komponentowe;
 
-import javax.swing.*;
-import java.awt.*;
-
-public class Window extends JFrame {
-
-    public Window() {
-        this.setBounds(50, 50, (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth() / 2, (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight() / 2);
-        this.setResizable(false);
-        this.setVisible(true);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
+public class Window {
 }
