cd9f16d08f20b1c05c6c1dd0c492f984b7872a5f
Patryk <marit3go@gmail.com>
2020-04-21 22:16:32 +0200

    Add polygons

--- src/pl/komponentowe/Controller.java
+++ src/pl/komponentowe/Controller.java
@@ -3,8 +3,8 @@ package pl.komponentowe;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
-
-import java.awt.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 
 public class Controller {
     @FXML
@@ -14,18 +14,22 @@ public class Controller {
     private ColorPicker colorPicker;
 
     @FXML
-    private Polygon l;
+    private Polygon left;
 
     @FXML
-    private Polygon r;
+    private Polygon right;
 
     public void method() {
         System.out.println("test");
         if ("test".equals(button.getText())) {
             button.setText("test Testowy");
+            left.setFill(Color.BLACK);
+            right.setFill(Color.BLACK);
 
         } else {
             button.setText("test");
+            left.setFill(Color.GREEN);
+            right.setFill(Color.GREEN);
         }
 
         button.setTextFill(colorPicker.getValue());
