e1ea93a8e29743748ceac4bf7a2843bc344bc029
Patryk <marit3go@gmail.com>
2020-04-28 19:35:37 +0200

    Refactor packages

--- src/pl/komponentowe/logic/Controller.java
+++ src/pl/komponentowe/logic/Controller.java
@@ -1,22 +1,18 @@
-package pl.komponentowe.presentation;
+package pl.komponentowe.logic;
 
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Polygon;
-
-import java.util.AbstractCollection;
-import java.util.Collection;
 
 public class Controller {
     @FXML
-    private Button button;
+    public Button button;
+
+    @FXML
+    private Button button2;
 
     @FXML
     private ColorPicker colorPicker;
@@ -31,8 +27,6 @@ public class Controller {
     private Pane pane;
 
     public void method() {
-        
-
         System.out.println("test");
         if ("test".equals(button.getText())) {
             button.setText("test Testowy");
@@ -46,14 +40,10 @@ public class Controller {
         }
 
         button.setTextFill(colorPicker.getValue());
+
     }
 
     public void onClickKey() {
-        pane.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent keyEvent) {
-                System.out.println(keyEvent.getCharacter());
-            }
-        });
+        button2.setText("Super!!!!");
     }
 }
--- src/pl/komponentowe/logic/Dashboard.java
+++ src/pl/komponentowe/logic/Dashboard.java
@@ -1,10 +1,7 @@
 package pl.komponentowe.logic;
 
-import javax.swing.*;
-
 public class Dashboard {
     private Speedometer speedometer;
-
 
     public Speedometer getSpeedometer() {
         return speedometer;
