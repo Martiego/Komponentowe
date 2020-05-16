feffac227054d0a6e9d74b2e0a1d4f6c4acb0e26
Kacper Świercz <kacper.swiercz00@gmail.com>
2020-05-16 11:13:43 +0200

    Create trip with each program run

--- src/pl/komponentowe/logic/Dashboard.java
+++ src/pl/komponentowe/logic/Dashboard.java
@@ -1,38 +1,139 @@
 package pl.komponentowe.logic;
 
-import pl.komponentowe.data.IOXml;
 import pl.komponentowe.data.Trip;
 import pl.komponentowe.logic.fluids.Fuel;
 import pl.komponentowe.logic.fluids.Oil;
-import pl.komponentowe.logic.lights.Indicators;
 
 import java.util.ArrayList;
+import java.util.Date;
 
-public class Dashboard {
-    private boolean highBeams;
+public class OnBoardComputer {
+    private Date date;
+    private double actualVelocity;
+    private double avgFuelConsumption;
+    private double maxVelocity;
+//    private double avgVelocity;
+    private double street;
+    private double mileage;
+//    private long time;
+    private Thread thread;
 
+    private double odometer;
+    private double tripMeter;
+
+    private Fuel fuel;
     private Oil oil;
-    private Indicators indicators;
-    private OnBoardComputer onBoardComputer;
 
-    public Dashboard() {
+    private double fuelConsumption;
+    private double timeFuelConsumption;
+
+
+    public OnBoardComputer() {
+        fuel = new Fuel(50);
+        fuel.fill(50);
         oil = new Oil(5);
-        onBoardComputer = new OnBoardComputer();
+        oil.fill(5);
+        date = new Date();
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                double avgStreet;
+                while (true) {
+                    try {
+                        Thread.sleep(100);
+                        avgStreet = actualVelocity / 3_600_000 * 100;
+                        // droga w km
+                        street += avgStreet;
+                        mileage += avgStreet;
+                        odometer += avgStreet;
+                        tripMeter += avgStreet;
+                    } catch (Exception ex) {
+                        System.out.println(ex.getMessage());
+                    }
+                }
+            }
+        });
+
+        thread.start();
     }
 
-    public Oil getOil() {
-        return oil;
+    public double getMileage() {
+        return mileage;
     }
 
-    public boolean isHighBeams() {
-        return highBeams;
+    public void accelerate() {
+        if (0 <= actualVelocity && 30 >= actualVelocity) {
+            actualVelocity += 0.50;
+        } else if (30 < actualVelocity && 60 >= actualVelocity) {
+            actualVelocity += 0.40;
+        } else if (60 < actualVelocity && 90 >= actualVelocity) {
+            actualVelocity += 0.35;
+        } else if (90 < actualVelocity && 160 >= actualVelocity) {
+            actualVelocity += 0.30;
+        } else if (160 < actualVelocity && 186 >= actualVelocity) {
+            actualVelocity += 0.20;
+        }
+
+        if (actualVelocity > maxVelocity) {
+            maxVelocity = actualVelocity;
+        }
+
+        timeFuelConsumption = Math.random() / 100;
+        fuelConsumption += timeFuelConsumption;
+
+        fuel.update(timeFuelConsumption);
+        oil.update(0.001);
     }
 
-    public Indicators getIndicators() {
-        return indicators;
+    public void decelerate(int value) {
+        if (0 < actualVelocity && 30 >= actualVelocity) {
+            actualVelocity -= value * 0.25;
+        } else if (30 < actualVelocity && 60 >= actualVelocity) {
+            actualVelocity -= value * 0.30;
+        } else if (60 < actualVelocity && 90 >= actualVelocity) {
+            actualVelocity -= value * 0.35;
+        } else if (90 < actualVelocity) {
+            actualVelocity -= value * 0.40;
+        } else if (0 >= actualVelocity) {
+            actualVelocity = 0;
+        }
     }
 
-    public OnBoardComputer getOnBoardComputer() {
-        return onBoardComputer;
+    public void resetOdometer() {
+        odometer = 0;
+    }
+
+    public double getActualVelocity() {
+        return actualVelocity;
+    }
+
+    public double getMaxVelocity() {
+        return maxVelocity;
+    }
+
+    public long getTime() {
+        return System.currentTimeMillis() - date.getTime();
+    }
+
+    public double getStreet() {
+        return street;
+    }
+
+    public double getOdometer() {
+        return odometer;
+    }
+
+    public double getAvgVelocity() {
+        double meter = street * 1000;
+        double seconds = (double)(getTime() / 1_000);
+        return meter / seconds * 36 / 10;
+    }
+
+    public double getAvgFuelConsumption() {
+        return fuelConsumption / street;
+    }
+
+    public Fuel getFuel() {
+        return fuel;
     }
 }
